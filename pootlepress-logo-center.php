3e2e07264058c6d7e1a2cc5d27bdf8ebc692ad3c
Nick Burne <nickburne@users.noreply.github.com>
2014-05-29 18:39:39 +0700

    Update pootlepress-logo-center.php

--- pootlepress-logo-center.php
+++ pootlepress-logo-center.php
@@ -3,7 +3,7 @@
 Plugin Name: Canvas Extension - Logo Inside Nav
 Plugin URI: http://pootlepress.com/canvas-extensions/
 Description: An extension for WooThemes Canvas that puts the logo in the center of the primary navigation.
-Version: 1.1.2
+Version: 1.1.3
 Author: PootlePress
 Author URI: http://pootlepress.com/
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -30,6 +30,6 @@ License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2
 	require_once( 'classes/class-pootlepress-logo-center.php' );
 
     $GLOBALS['pootlepress_center_logo'] = new Pootlepress_Center_logo( __FILE__ );
-    $GLOBALS['pootlepress_center_logo']->version = '1.1.2';
+    $GLOBALS['pootlepress_center_logo']->version = '1.1.3';
 	
 ?>
